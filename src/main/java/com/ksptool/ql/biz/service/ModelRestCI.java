0c82015ff832b731c322e1f4da08d6dce162cebd
KspTooi <ksptooi@outlook.com>
2025-03-17 02:10:30 +0800

    feat:模型统一调用接口

---
+++ src/main/java/com/ksptool/ql/biz/service/ModelRestCI.java
@@ -0,0 +1,20 @@
+package com.ksptool.ql.biz.service;
+
+import com.ksptool.ql.biz.model.dto.ModelChatParam;
+import com.ksptool.ql.biz.model.vo.ModelChatContext;
+import com.ksptool.ql.commons.exception.BizException;
+import okhttp3.OkHttpClient;
+
+import java.util.function.Consumer;
+
+/**
+ * 模型Rest通用接入接口
+ * ModelRestCommonInterface
+ */
+public interface ModelRestCI {
+
+    public String sendMessageSync(OkHttpClient client, ModelChatParam dto) throws BizException;
+
+    void sendMessageStream(OkHttpClient client, ModelChatParam param, Consumer<ModelChatContext> callback);
+
+}
