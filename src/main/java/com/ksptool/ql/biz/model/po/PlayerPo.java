251b1ec2d24d66f95e3792cd012dd81daac18e41
KspTooi <ksptooi@outlook.com>
2025-05-04 00:20:41 +0800

    feat:Player

---
+++ src/main/java/com/ksptool/ql/biz/model/po/PlayerPo.java
@@ -0,0 +1,74 @@
+package com.ksptool.ql.biz.model.po;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.Comment;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "player")
+public class PlayerPo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Comment("主键ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id",nullable = false,foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    @Comment("关联用户ID")
+    private UserPo user;
+
+    @Column(name = "avatar_url", length = 320)
+    @Comment("头像路径")
+    private String avatarUrl;
+
+    @Column(name = "name", nullable = false,unique = true, length = 64)
+    @Comment("(明文)人物角色名称")
+    private String name;
+
+    @Column(length = 40000)
+    @Comment("(明文)个人信息")
+    private String publicInfo;
+
+    @Column(length = 32000)
+    @Comment("(密文)人物角色描述")
+    private String description;
+
+    @Column(name = "balance",nullable = false)
+    @Comment("钱包余额(CU)")
+    private BigDecimal balance;
+
+    @Column(name = "language",nullable = false,length = 24)
+    @Comment("语言 如 中文,English,zh-CN,en-US")
+    private String language;
+
+    @Column(name = "era",length = 32)
+    @Comment("年代 如 古代,中世纪,现代,未来,赛博朋克,80S,90S,2025-01-01")
+    private String era;
+
+    @Column(name = "content_filter_level")
+    @Comment("内容过滤等级 0:不过滤 1:普通 2:严格")
+    private Integer contentFilterLevel;
+
+    @Column(name = "active", nullable = false)
+    @Comment("是否激活 0:是 1:否")
+    private Integer active;
+
+    @Column(name = "create_time", nullable = false, updatable = false)
+    @Comment("创建时间")
+    private Date createTime;
+
+    @Column(name = "update_time", nullable = false)
+    @Comment("修改时间")
+    private Date updateTime;
+
+    @Column(name = "last_active_time", nullable = false)
+    @Comment("最后激活时间")
+    private Date lastActiveTime;
+
+}
