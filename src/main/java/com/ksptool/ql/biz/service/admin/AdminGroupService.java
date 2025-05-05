170896a78edc5e9e242341a4ef4eeea19828f854
KspTooi <ksptooi@outlook.com>
2025-05-05 21:29:27 +0800

    feat:优化权限节点、访问组排序

--- src/main/java/com/ksptool/ql/biz/service/admin/AdminGroupService.java
+++ src/main/java/com/ksptool/ql/biz/service/admin/AdminGroupService.java
@@ -16,6 +16,7 @@ import com.ksptool.ql.commons.exception.BizException;
 import com.ksptool.ql.commons.web.RestPageableView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -42,7 +43,7 @@ public class AdminGroupService {
     private AuthService authService;
 
     public List<GetGroupDefinitionsVo> getGroupDefinitions(){
-        List<GroupPo> pos = repository.findAll();
+        List<GroupPo> pos = repository.findAll(Sort.by(Sort.Direction.DESC, "createTime"));
         return as(pos,GetGroupDefinitionsVo.class);
     }
 
--- src/main/java/com/ksptool/ql/biz/service/admin/AdminPermissionService.java
+++ src/main/java/com/ksptool/ql/biz/service/admin/AdminPermissionService.java
@@ -14,6 +14,7 @@ import com.ksptool.ql.commons.web.RestPageableView;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -31,7 +32,7 @@ public class AdminPermissionService {
     private PermissionRepository repository;
 
     public List<GetPermissionDefinitionVo> getPermissionDefinition(){
-        List<PermissionPo> pos = repository.findAll();
+        List<PermissionPo> pos = repository.findAll(Sort.by(Sort.Direction.DESC, "createTime"));
         return as(pos,GetPermissionDefinitionVo.class);
     }
 
