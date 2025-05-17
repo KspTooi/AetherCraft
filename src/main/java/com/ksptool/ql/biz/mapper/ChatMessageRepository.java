e70a5bc0782591cad2fac9448b48a9e7026d84bf
KspTooi <ksptooi@outlook.com>
2025-05-18 05:10:25 +0800

    fix:修复count查询

--- src/main/java/com/ksptool/ql/biz/mapper/ChatMessageRepository.java
+++ src/main/java/com/ksptool/ql/biz/mapper/ChatMessageRepository.java
@@ -4,6 +4,8 @@ import com.ksptool.ql.biz.model.po.ChatMessagePo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -15,5 +17,9 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessagePo, Long
     @Modifying
     Long removeByThreadId(Long threadId);
 
-    int getCountByThreadId(Long threadId);
+    @Query("""
+        SELECT COUNT(DISTINCT cm.id) FROM ChatMessagePo cm
+        WHERE cm.thread.id = :tid
+    """)
+    int getCountByThreadId(@Param("tid") Long tid);
 }
