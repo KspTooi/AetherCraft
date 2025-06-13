36685bdec39d3bb4aceec75cc958c6325beff8aa
KspTooi <ksptooi@outlook.com>
2025-06-14 02:42:37 +0800

    feat:NPC对话示例增加日志打印、过滤敏感字段

--- src/main/java/com/ksptool/ql/biz/controller/NpcChatExampleController.java
+++ src/main/java/com/ksptool/ql/biz/controller/NpcChatExampleController.java
@@ -4,6 +4,7 @@ import com.ksptool.ql.biz.model.dto.CommonIdDto;
 import com.ksptool.ql.biz.model.dto.SaveNpcChatExampleDto;
 import com.ksptool.ql.biz.model.vo.GetNpcChatExampleListVo;
 import com.ksptool.ql.biz.service.NpcChatExampleService;
+import com.ksptool.ql.commons.annotation.PrintLog;
 import com.ksptool.ql.commons.exception.BizException;
 import com.ksptool.ql.commons.web.Result;
 import jakarta.validation.Valid;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
+@PrintLog
 @RestController
 @RequestMapping("/npc/chatExample")
 public class NpcChatExampleController {
@@ -21,11 +23,13 @@ public class NpcChatExampleController {
     @Autowired
     private NpcChatExampleService service;
 
+    @PrintLog(sensitiveFields = "data")
     @PostMapping("/getNpcChatExampleList")
     public Result<List<GetNpcChatExampleListVo>> getModelRoleList(@RequestBody @Valid CommonIdDto dto) throws BizException {
         return Result.success(service.getModelRoleList(dto.getId()));
     }
 
+    @PrintLog(sensitiveFields = "examples")
     @PostMapping("saveNpcChatExample")
     public Result<String> saveNpcChatExample(@RequestBody @Valid SaveNpcChatExampleDto dto) throws BizException {
         service.saveNpcChatExample(dto);
